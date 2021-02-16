d19f252cd2bea0e58601a49b62b126801cb9f260
maxfri <maxfri1995@gmail.com>
2021-02-17 01:54:40 +0300

    feat: add decisions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
+exports.min = function min(array) {
+    if (arguments.length == 0 || array.length == 0) {
+        return 0;
+    } else {
+        let result = array.reduce(function (min, current) {
+            if (min > current) {
+                return (min = current);
+            } else {
+                return min;
+            }
+        }, 0);
+        return result;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (arguments.length == 0 || array.length == 0) {
+        return 0;
+    } else {
+        let result = array.reduce(function (max, current) {
+            if (max < current) {
+                return (max = current);
+            } else {
+                return max;
+            }
+        }, 0);
+        return result;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (arguments.length == 0 || array.length == 0) {
+        return 0;
+    } else {
+        let result = array.reduce(function (avr, current) {
+            return avr + current;
+        }, 0);
+        return result / array.length;
+    }
+};
